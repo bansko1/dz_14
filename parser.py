a3b61227aaba291fc67218b02d9ca7496efb8918
Alex <alexvp@bk.ru>
2023-07-28 20:10:53 +0300

    add telegram bot functional

--- parser.py
+++ parser.py
@@ -5,41 +5,48 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-domain = 'https://www.mos.ru'
-url = f'{domain}/news'
 
-result = {}
+def parce():
+    domain = 'https://www.mos.ru'
+    url = f'{domain}/news'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+    result = {}
 
-tags_news = soup.find_all('a', class_='commonCard__link link-black')
-for tag in tags_news:
-    # print(tag.text)
-    text = tag.text
-    href = tag.get('href')
-    # print(text, href)
-    url = f'{domain}{href}'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    news = soup.find_all('p')
-    # print(news)                                     # Проверка наличия новости в тегах
-    text_list = []
-    for part in news:
-        # print(part.text)
 
-        part_text = part.text.replace('\xa0', '\n ')
-        # print(part_text)
-        text_list.append(part_text)
-    # print(text_list)
-    text_list = ' '.join(text_list)
-    if text_list:
-        result[text] = text_list
-# print(result)
-with open('result.json', 'w') as f:
-    json.dump(result, f)
-numb = result.items()
-for i in range(len(result)):
-    print('Новость', i)
-    print('Заголовок: ', list(numb)[i][0])
-    print('Текст: ', list(numb)[i][1])
+    tags_news = soup.find_all('a', class_='commonCard__link link-black')
+    for tag in tags_news:
+        # print(tag.text)
+        text = tag.text
+        href = tag.get('href')
+        # print(text, href)
+        url = f'{domain}{href}'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        news = soup.find_all('p')
+        # print(news)                                     # Проверка наличия новости в тегах
+        text_list = []
+        for part in news:
+            # print(part.text)
+
+            part_text = part.text.replace('\xa0', '\n ')
+            # print(part_text)
+            text_list.append(part_text)
+        # print(text_list)
+        text_list = ' '.join(text_list)
+        if text_list:
+            result[text] = text_list
+    # print(result)
+    with open('result.json', 'w') as f:
+        json.dump(result, f)
+    numb = result.items()
+    return numb
+    # for i in range(len(result)):
+    #     print('Новость', i)
+    #     print('Заголовок: ', list(numb)[i][0])
+    #     print('Текст: ', list(numb)[i][1])
+
+
+if __name__ == '__main__':
+    parce()
